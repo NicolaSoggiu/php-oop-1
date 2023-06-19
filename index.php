f0381b0aa2290fec84d0dced9f69a4cc821c4e05
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-19 19:03:13 +0200

    add first style

--- index.php
+++ index.php
@@ -12,18 +12,28 @@ include __DIR__ . "/db.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"
+        defer></script>
 </head>
 
 <body>
+    <div class="container">
 
-    <h1>MOVIES</h1>
-    <ul>
-        <?php foreach ($movies as $movie) : ?>
-        <li>
-            <?php $movie->printMovie(); ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+        <h1 class="text-center text-danger mb-4">MOVIES</h1>
+
+        <ul class="cards-container d-flex justify-content-between flex-wrap">
+            <?php foreach ($movies as $movie) : ?>
+            <li class="mb-5">
+                <?php $movie->printMovie(); ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+
+    </div>
 </body>
 
 </html>
