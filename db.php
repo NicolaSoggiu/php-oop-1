37c93cdf5d2d2235da80c3d28404f69946bc0e8f
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-19 18:51:54 +0200

    add more movie (no style )

--- db.php
+++ db.php
@@ -14,4 +14,32 @@ $movies = [
         "136 min",
         new Genres(['Sporty', 'Drammatic', 'Comedy'])
     ),
+    new Movies(
+        "Slevin Patto Criminale",
+        "Paul McGuigan",
+        "2006",
+        "109 min",
+        new Genres(['Thriller', 'Drammatic', 'Comedy'])
+    ),
+    new Movies(
+        "Blade Runner",
+        "Ridley Scott",
+        "1982",
+        "124 min",
+        new Genres(['Sci-fi', 'Action', 'Thriller'])
+    ),
+    new Movies(
+        "The Big Short",
+        "Adam McKay",
+        "2015",
+        "130 min",
+        new Genres(['Biographical', 'Comedy', "Drammatic"])
+    ),
+    new Movies(
+        "American Hustle",
+        "David O.Russel",
+        "2013",
+        "138 min",
+        new Genres(['Comedy', 'Trhiller', "Drammatic"])
+    ),
 ];
